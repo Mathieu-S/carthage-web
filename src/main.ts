14a93cf8bb00eb53983b1da02a83439cd83efcc0
Mathieu-S <mathieu-S@git.com>
2020-02-28 11:41:55 +0100

    Register VueIocPlugin in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { VueIocPlugin } from "@vue-ioc/core";
+
 import "normalize.css";
 
 Vue.config.productionTip = false;
+Vue.use(VueIocPlugin);
 
 new Vue({
   router,
